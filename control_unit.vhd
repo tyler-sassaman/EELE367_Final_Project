f542cb3fada39eab4e07f432a0d649675d76e67b
tyler-sassaman <semperfi@ronan.net>
2018-04-25 13:53:34 -0600

    Added more instructions

--- control_unit.vhd
+++ control_unit.vhd
@@ -25,8 +25,11 @@ architecture control_unit_arch of control_unit is
 	type state_type is (S_FETCH_0, S_FETCH_1, S_FETCH_2,
 			    S_DECODE_3,
 			    S_LDA_IMM_4, S_LDA_IMM_5, S_LDA_IMM_6,
+			    S_LDB_IMM_4, S_LDB_IMM_5, S_LDB_IMM_6,
 			    S_LDA_DIR_4, S_LDA_DIR_5, S_LDA_DIR_6, S_LDA_DIR_7, S_LDA_DIR_8,
+			    S_LDB_DIR_4, S_LDB_DIR_5, S_LDB_DIR_6, S_LDB_DIR_7, S_LDB_DIR_8,
 			    S_STA_DIR_4, S_STA_DIR_5, S_STA_DIR_6, S_STA_DIR_7,
+			    S_STB_DIR_4, S_STB_DIR_5, S_STB_DIR_6, S_STB_DIR_7,
 			    S_ADD_AB_4,
 			    S_BRA_4, S_BRA_5, S_BRA_6,
 			    S_BEQ_4, S_BEQ_5, S_BEQ_6, S_BEQ_7);
@@ -78,11 +81,17 @@ architecture control_unit_arch of control_unit is
 		elsif(current_state = S_DECODE_3) then
 
 			if(IR = LDA_IMM) then
-				next_state <= S_LDA_IMM_4;		-- Load Immediate
+				next_state <= S_LDA_IMM_4;		-- Load A Immediate
 			elsif(IR = LDA_DIR) then
-				next_state <= S_LDA_DIR_4;		-- Load Direct
+				next_state <= S_LDA_DIR_4;		-- Load A Direct
 			elsif(IR = STA_DIR) then
 				next_state <= S_STA_DIR_4;		-- Store A Direct
+			elsif(IR = LDB_IMM) then	
+				next_state <= S_LDB_IMM_4;		-- Load B Immediate
+			elsif(IR = LDB_DIR) then
+				next_state <= S_LDB_IMM_4;		-- Load B Direct
+			elsif(IR = STB_DIR) then
+				next_state <= S_LDB_IMM_4;		-- Store B Direct
 			elsif(IR = ADD_AB) then
 				next_state <= S_ADD_AB_4;		-- Add A and B
 			elsif(IR = BRA) then
@@ -106,6 +115,16 @@ architecture control_unit_arch of control_unit is
 			next_state <= S_FETCH_0; 
 
 		---------------------------------------
+		--              LDB_IMM              --
+		---------------------------------------
+		elsif(current_state = S_LDB_IMM_4) then
+			next_state <= S_LDB_IMM_5;
+		elsif(current_state = S_LDB_IMM_5) then
+			next_state <= S_LDB_IMM_6;
+		elsif(current_state = S_LDB_IMM_6) then
+			next_state <= S_FETCH_0; 
+
+		---------------------------------------
 		--              LDA_DIR              --
 		---------------------------------------
 		elsif(current_state = S_LDA_DIR_4) then
@@ -120,6 +139,20 @@ architecture control_unit_arch of control_unit is
 			next_state <= S_FETCH_0;
 
 		---------------------------------------
+		--              LDB_DIR              --
+		---------------------------------------
+		elsif(current_state = S_LDB_DIR_4) then
+			next_state <= S_LDB_DIR_5;
+		elsif(current_state = S_LDB_DIR_5) then
+			next_state <= S_LDB_DIR_6;
+		elsif(current_state = S_LDB_DIR_6) then
+			next_state <= S_LDB_DIR_7;
+		elsif(current_state = S_LDB_DIR_7) then
+			next_state <= S_LDB_DIR_8;
+		elsif(current_state = S_LDB_DIR_8) then
+			next_state <= S_FETCH_0;
+
+		---------------------------------------
 		--              STA_DIR              --
 		--------------------------------------- 
 		elsif(current_state = S_STA_DIR_4) then
@@ -129,6 +162,18 @@ architecture control_unit_arch of control_unit is
 		elsif(current_state = S_STA_DIR_6) then
 			next_state <= S_STA_DIR_7;
 		elsif(current_state = S_STA_DIR_7) then
+			next_state <= S_FETCH_0; 
+
+		---------------------------------------
+		--              STB_DIR              --
+		--------------------------------------- 
+		elsif(current_state = S_STB_DIR_4) then
+			next_state <= S_STB_DIR_5;
+		elsif(current_state = S_STB_DIR_5) then
+			next_state <= S_STB_DIR_6;
+		elsif(current_state = S_STB_DIR_6) then
+			next_state <= S_STB_DIR_7;
+		elsif(current_state = S_STB_DIR_7) then
 			next_state <= S_FETCH_0; 
 
 		---------------------------------------
@@ -254,6 +299,45 @@ architecture control_unit_arch of control_unit is
 				PC_Inc <= '0';
 				A_Load <= '1';
 				B_Load <= '0';
+				ALU_Select <= "000";
+				CCR_Load <= '0';
+				Bus1_Sel <= "00"; -- "00" = PC, "01" = A, "10" = B
+				Bus2_Sel <= "10"; -- "00" = ALU_Result, "01" = Bus1, "10" = from_memory
+				writeEn <= '0';
+
+			when S_LDB_IMM_4 => 
+				IR_Load <= '0';
+				MAR_Load <= '1';
+				PC_Load <= '0';
+				PC_Inc <= '0';
+				A_Load <= '0';
+				B_Load <= '0';
+				ALU_Select <= "000";
+				CCR_Load <= '0';
+				Bus1_Sel <= "00"; -- "00" = PC, "01" = A, "10" = B
+				Bus2_Sel <= "01"; -- "00" = ALU_Result, "01" = Bus1, "10" = from_memory
+				writeEn <= '0';
+
+			when S_LDB_IMM_5 => 
+				IR_Load <= '0';
+				MAR_Load <= '0';
+				PC_Load <= '0';
+				PC_Inc <= '1';
+				A_Load <= '0';
+				B_Load <= '0';
+				ALU_Select <= "000";
+				CCR_Load <= '0';
+				Bus1_Sel <= "00"; -- "00" = PC, "01" = A, "10" = B
+				Bus2_Sel <= "00"; -- "00" = ALU_Result, "01" = Bus1, "10" = from_memory
+				writeEn <= '0';
+
+			when S_LDB_IMM_6 => 
+				IR_Load <= '0';
+				MAR_Load <= '0';
+				PC_Load <= '0';
+				PC_Inc <= '0';
+				A_Load <= '0';
+				B_Load <= '1';
 				ALU_Select <= "000";
 				CCR_Load <= '0';
 				Bus1_Sel <= "00"; -- "00" = PC, "01" = A, "10" = B
